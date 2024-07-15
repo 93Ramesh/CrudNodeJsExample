b8b4ebd9f56d8dcd598a57658ebc4e0530040c21
Ramesh Bhoyar <144668273+93Ramesh@users.noreply.github.com>
2024-07-15 19:01:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import connectDB from './config/database.js'; // Adjust this path if necessary
-import gymRoutes from './routes/gymRoute.js'; // Adjust this path if necessary
+import connectDB from './config/database.js'; 
+import gymRoutes from './routes/gymRoute.js'; 
 
 dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(bodyParser.json());
 
-// Routes
 app.use('/api', gymRoutes);
 
 // Connect to MongoDB
@@ -22,5 +20,5 @@ connectDB().then(() => {
   });
 }).catch(err => {
   console.error('Error connecting to MongoDB:', err);
-  process.exit(1); // Exit process on connection failure
+  process.exit(1);
 });
